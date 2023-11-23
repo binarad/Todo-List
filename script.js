f5067a24c71ee09c9fef9a4ecda5f3f60bd33a6f
binarad <pozhidaev.ruslan@gmail.com>
2023-11-23 15:57:22 +0200

    todo

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function myFunc() {
-    let text = document.getElementById("title").innerHTML;
-    document.getElementById("title").innerHTML = text.toUpperCase();
+function addTask(){
+    if(inputBox.value === ''){
+        alert("giga niga!")
+    }
+    else{
+        let li = document.createElement("li")
+        li.innerText = inputBox.value;
+        listContainer.appendChild(li);         
+    }
+    inputBox.value = '';
 }
