ea5b2d858fa7235955e95a99569df2f00ab5c406
binar <pozhidaev.ruslan@gmail.com>
2023-11-24 11:02:13 +0200

    todo

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 const inputBox = document.querySelector("#input-box");
 const listContainer = document.querySelector("#list-container");
 
-<<<<<<< HEAD
-function addtask(){
-    if(inputBox.value == ''){
-        alert("You must write something!");
-    }
-    else{
-        let li = document.createElement("li");
-        li.innerText = inputBox.value;
-        listContainer.appendChild(li);
-    }
-}
-=======
 function addTask(){
     if(inputBox.value === ''){
         alert("giga niga!")
@@ -24,4 +12,3 @@ function addTask(){
     }
     inputBox.value = '';
 }
->>>>>>> f5067a24c71ee09c9fef9a4ecda5f3f60bd33a6f
