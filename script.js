c34e2e184e8ed370a18edf71c99b381632464dcf
binar <pozhidaev.ruslan@gmail.com>
2023-11-25 16:15:01 +0200

    todo

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
 const inputBox = document.querySelector("#input-box");
 function addTask(){
-if(inputBox.value === "") {
-    alert("Please write the task!")
-    return;
-}
+    if(inputBox.value === "") {
+        alert("Please write the task!")
+        return;
+    }
 
-let taskList = document.getElementById("todo-list");
-let taskItem = document.createElement("li")
-taskItem.className = 'task';
+    let taskList = document.getElementById("todo-list");
+    let taskItem = document.createElement("li")
+    taskItem.className = 'task';
 
-let checkbox = document.createElement("input")
-checkbox.type = 'checkbox';
-checkbox.className ='check'
-checkbox.addEventListener('change', function(){
-    toggleTaskCompletion(this);
-})
+    let checkbox = document.createElement("input")
+    checkbox.type = 'checkbox';
+    checkbox.className ='check'
+    checkbox.addEventListener('change', function(){
+        toggleTaskCompletion(this);
+    })
 
-let editButton = document.createElement("button")
-editButton.textContent = 'Edit';
-editButton.className = 'edit';
-editButton.addEventListener('click', function(){
-    editTask(checkbox);
-})
+    let taskLabel = document.createElement('label');
+    taskLabel.textContent = inputBox.value;
 
-let taskLabel = document.createElement('label');
-taskLabel.textContent = inputBox.value;
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'del';
+    deleteButton.addEventListener('click', function(){
+        deleteTask(taskItem);
+    })
 
-let deleteButton = document.createElement("button");
-deleteButton.textContent = 'Delete';
-deleteButton.className = 'del';
-deleteButton.addEventListener('click', function(){
-    deleteTask(taskItem);
-})
+    let editButton = document.createElement('button')
+		editButton.textContent = 'Edit'
+		editButton.className = 'edit'
+		editButton.addEventListener('click', function () {
+			editTask(taskLabel)
+		})
 
-taskItem.appendChild(checkbox);
-taskItem.appendChild(taskLabel);
-taskItem.appendChild(deleteButton);
-taskItem.appendChild(editButton);
 
-taskList.appendChild(taskItem);
+    taskItem.appendChild(checkbox);
+    taskItem.appendChild(taskLabel);
+    taskItem.appendChild(deleteButton);
+    taskItem.appendChild(editButton);
 
-inputBox.value = '';
+    taskList.appendChild(taskItem);
+
+    inputBox.value = '';
 }
 function toggleTaskCompletion(checkbox) {
     let taskLabel = checkbox.nextElementSibling;
@@ -57,9 +58,9 @@ function deleteTask(taskItem) {
     taskList.removeChild(taskItem);
 }
 
-function editTask(checkbox) {
-    let newText = prompt("Write new text: ", checkbox.value);
+function editTask(taskLabel) {
+    let newText = prompt("Write new text:", taskLabel.textContent);
     if(newText != null){
-        checkbox.value = newText;
+        taskLabel.textContent = newText;
     }
 }
