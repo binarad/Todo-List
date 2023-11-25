73c906f6089a498f4af4ffad803dbb0968940887
binarad <pozhidaev.ruslan@gmail.com>
2023-11-25 04:11:47 +0200

    todo

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
 const inputBox = document.querySelector("#input-box");
-const listContainer = document.querySelector("#list-container");
-
 function addTask(){
-    if(inputBox.value === ''){
-        alert("giga niga!")
+if(inputBox.value === "") {
+    alert("Please write the task!")
+    return;
+}
+
+let taskList = document.getElementById("todo-list");
+let taskItem = document.createElement("div")
+taskItem.className = 'task';
+
+let checkbox = document.createElement("input")
+checkbox.type = 'checkbox';
+checkbox.className ='check'
+checkbox.addEventListener('change', function(){
+    toggleTaskCompletion(this);
+})
+
+let editButton = document.createElement("button")
+editButton.textContent = 'Edit';
+editButton.className = 'edit';
+editButton.addEventListener('click', function(){
+    editTask(checkbox);
+})
+
+let taskLabel = document.createElement('label');
+taskLabel.textContent = inputBox.value;
+
+let deleteButton = document.createElement("button");
+deleteButton.textContent = 'Delete';
+deleteButton.className = 'del';
+deleteButton.addEventListener('click', function(){
+    deleteTask(taskItem);
+})
+
+taskItem.appendChild(checkbox);
+taskItem.appendChild(taskLabel);
+taskItem.appendChild(deleteButton);
+taskItem.appendChild(editButton);
+
+taskList.appendChild(taskItem);
+
+inputBox.value = '';
+}
+function toggleTaskCompletion(checkbox) {
+    let taskLabel = checkbox.nextElementSibling;
+    if(checkbox.checked) {
+        taskLabel.style.textDecoration = 'line-through';
     }
-    else{
-        let li = document.createElement("li")
-        li.innerText = inputBox.value;
-        listContainer.appendChild(li);         
+    else {
+        taskLabel.style.textDecoration = 'none';
     }
-    inputBox.value = '';
+}
+
+function deleteTask(taskItem) {
+    let taskList = document.getElementById("todo-list");
+    taskList.removeChild(taskItem);
+}
+
+function editTask(checkbox) {
+    let newText = prompt("Write new text: ", checkbox.value);
+    if(newText != null){
+        checkbox.value = newText;
+    }
 }
